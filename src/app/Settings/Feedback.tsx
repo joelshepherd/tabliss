adda864b8401f3a4d0aa7a34d1e71a7b7d1cec50
Joel Shepherd <joel@swivid.ink>
2017-07-30 18:36:41 +1000

    Converting feedback to a form

--- src/app/Settings/Feedback.tsx
+++ src/app/Settings/Feedback.tsx
@@ -1,12 +1,49 @@
 import * as React from 'react';
-import { Component } from 'react';
 
-class Feedback extends Component {
+interface State {
+  body: string;
+}
+
+class Feedback extends React.Component<{}, State> {
+  constructor() {
+    super();
+
+    this.state = {
+      body: '',
+    };
+  }
+
+  send() {
+    fetch(`https://formspree.io/joel@swivid.ink`, {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ...this.state }),
+    })
+      .then(res => {
+        if (res.ok) {
+          alert('Thank you for your feedback!');
+        } else {
+          alert('Unable to send your feedback :(');
+        }
+      });
+  }
+
   render() {
     return (
       <div>
         <h3>Feedback</h3>
-        <p><a href="mailto:joel@swivid.ink?subject=Start+feedback">Send feedback or suggestions</a></p>
+        <label>
+          <textarea
+            rows={3}
+            placeholder="Give feedback or suggestions"
+            value={this.state.body}
+            onChange={event => this.setState({ body: event.target.value })}
+          />
+        </label>
+        <button onClick={() => this.send()}>Send</button>
       </div>
     );
   }
