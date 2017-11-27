0f817bb7df364b9dcc062f31cac79441fe8ac5c6
Joel Shepherd <joel@swivid.ink>
2017-11-27 19:39:44 +1000

    Minor styling update

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,14 @@ import { Provider } from 'react-redux';
 import { App } from './app';
 import { store } from './data';
 
+// Setup error logging
 if (process.env.NODE_ENV === 'production' && process.env.SENTRY_PUBLIC_DSN) {
-  require('raven-js').config(process.env.SENTRY_PUBLIC_DSN).install();
+  require('raven-js').config(process.env.SENTRY_PUBLIC_DSN, {
+    release: '1.6.0',
+  }).install();
 }
 
+// Render app into root element
 render(
   <Provider store={store}>
     <App />
@@ -15,6 +19,7 @@ render(
   document.getElementById('root'),
 );
 
+// Register service worker on web
 if (process.env.BUILD_TARGET === 'web') {
   require('./serviceWorker').register();
 }
