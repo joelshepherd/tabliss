2702049d12030f80aa975d25b3e7b3fda675920b
Joel Shepherd <joel@swivid.ink>
2018-02-10 23:35:50 +1000

    Fixing 12 hour time in some locales

--- src/plugins/core/widgets/time/Digital.tsx
+++ src/plugins/core/widgets/time/Digital.tsx
@@ -6,12 +6,18 @@ interface Props {
   time: DateTime;
 }
 
+const TIME_12_SIMPLE = {
+  hour: 'numeric',
+  minute: '2-digit',
+  hour12: true,
+};
+
 const Digital: React.StatelessComponent<Props> = ({ time, hour12 }) => {
   return (
     <div className="Time Digital">
       <h1>
         {time.toLocaleString(hour12
-          ? DateTime.TIME_SIMPLE
+          ? TIME_12_SIMPLE
           : DateTime.TIME_24_SIMPLE
         )}
       </h1>
