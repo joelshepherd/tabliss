7bd527583f27696518312951aa22d038246941a2
Joel Shepherd <joel@swivid.ink>
2018-02-21 17:44:56 +1000

    Adding github link

--- src/app/settings/Settings.tsx
+++ src/app/settings/Settings.tsx
@@ -5,7 +5,7 @@ import Background from './Background';
 import Feedback from './Feedback';
 import Widgets from './Widgets';
 import { Action, resetDashboard, toggleSettings } from '../../data';
-import { globeIcon, twitterIcon } from '../ui';
+import { githubIcon, globeIcon, twitterIcon } from '../ui';
 import './Settings.sass';
 const logo = require('./logo.svg');
 const ESCAPE_KEY = 27;
@@ -44,6 +44,8 @@ class Settings extends React.PureComponent<Props> {
             <a href="https://tabliss.io/" target="_blank">{globeIcon}</a>
             &nbsp;&nbsp;
             <a href="https://twitter.com/tabliss" target="_blank" rel="noopener noreferrer">{twitterIcon}</a>
+            &nbsp;&nbsp;
+            <a href="https://github.com/joelshepherd/tabliss" target="_blank" rel="noopener noreferrer">{githubIcon}</a>
           </p>
 
           <FormattedMessage
--- src/app/ui/icons/icons.tsx
+++ src/app/ui/icons/icons.tsx
@@ -7,6 +7,7 @@ const minus = require('feather-icons/dist/icons/minus.svg');
 const plus = require('feather-icons/dist/icons/plus.svg');
 const trash2 = require('feather-icons/dist/icons/trash-2.svg');
 const twitter = require('feather-icons/dist/icons/twitter.svg');
+const github = require('feather-icons/dist/icons/github.svg');
 
 export const arrowDownIcon = <i dangerouslySetInnerHTML={{ __html: arrowDown }} />;
 export const arrowUpIcon = <i dangerouslySetInnerHTML={{ __html: arrowUp }} />;
@@ -15,3 +16,4 @@ export const collapseIcon = <i dangerouslySetInnerHTML={{ __html: minus }} />;
 export const expandIcon = <i dangerouslySetInnerHTML={{ __html: plus }} />;
 export const removeIcon = <i dangerouslySetInnerHTML={{ __html: trash2 }} />;
 export const twitterIcon = <i dangerouslySetInnerHTML={{ __html: twitter }} />;
+export const githubIcon = <i dangerouslySetInnerHTML={{ __html: github }} />;
