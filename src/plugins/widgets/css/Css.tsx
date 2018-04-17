77be058d254d41abaa2561a382a397154e027ec3
Steven Harrigan <def.monk0@gmail.com>
2018-04-16 21:46:37 -0400

    Added new widget for custom CSS. It works!

---
+++ src/plugins/widgets/css/Css.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import './Css.sass';
+
+interface Props {
+  input?: string;
+}
+
+class Css extends React.PureComponent<Props> {
+  static defaultProps = {
+    input: '',
+  };
+
+  componentDidMount() {
+    this.attach();
+  }
+
+  componentDidUpdate() {
+    this.attach();
+  }
+
+  componentWillUnmount() {
+    this.detach();
+  }
+
+  render() {
+    return null;
+  }
+
+  private detach() {
+    const head = document.head as HTMLElement;
+    var style = document.getElementById('CustomCss') as HTMLElement;
+
+    head.removeChild(style);
+  }
+
+  private attach() {
+    const head = document.head as HTMLElement;
+    var style = document.createElement('style');
+
+    style.id = 'CustomCss';
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(this.props.input || ''));
+
+    head.appendChild(style);
+  }
+}
+
+export default Css;
---
+++ src/plugins/widgets/css/index.ts
@@ -0,0 +1,2 @@
+export { default as Css } from './Css';
+export { default as CssSettings } from './CssSettings';
