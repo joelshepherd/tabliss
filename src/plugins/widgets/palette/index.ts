e198f16da5d9b931587dd3ed6a6fb774ce0e12f0
sigfried <rssanjuan2704@gmail.com>
2019-08-25 19:11:34 -0500

    [ADD] Initial widget definition.

---
+++ src/plugins/widgets/palette/index.ts
@@ -0,0 +1,11 @@
+import { Config } from '../../types';
+import Palette from './Palette';
+
+const config: Config = {
+  key: 'widget/palette',
+  name: 'Random color palettes',
+  description: 'Be inspired with this beautiful random color palettes.',
+  dashboardComponent: Palette,
+};
+
+export default config;
