3dd14c178c5b095b08a234233e3d32c3057a3398
TrickyPR <trickypr@users.noreply.github.com>
2020-02-17 12:00:41 +1100

    Restyle custom css settings

--- src/plugins/widgets/css/CssSettings.tsx
+++ src/plugins/widgets/css/CssSettings.tsx
@@ -1,23 +1,24 @@
 import React, { FC } from 'react';
 
 import { Props, defaultData } from './types';
+import { Label, Input, Alert } from 'reactstrap';
 
 const CssSettings: FC<Props> = ({ data = defaultData, setData }) => (
   <div className="CssSettings">
-    <label>
-      CSS Snippet
-      <textarea
-        rows={3}
-        style={{ fontFamily: 'monospace' }}
-        value={data.input}
-        onChange={event => setData({ input: event.target.value })}
-      />
-    </label>
+    <Label for="CustomCssTextarea">CSS Snippet</Label>
+    <Input
+      type="textarea"
+      id="CustomCssTextarea"
+      rows={3}
+      style={{ fontFamily: 'monospace' }}
+      value={data.input}
+      onChange={event => setData({ input: event.target.value })}
+    />
 
-    <p className="info">
+    <Alert color="warning">
       Warning: this functionality is intended for advanced users. Custom styles
       may break at any time.
-    </p>
+    </Alert>
   </div>
 );
 
