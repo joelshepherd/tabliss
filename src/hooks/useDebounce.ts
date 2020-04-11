0b6ee97e7a27eef6d5d01a8be10c2a4abcdf31f2
Ricky Gonzales <gonzalrick@gmail.com>
2020-04-11 15:27:54 +1000

    Added debounceInput component

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-export function useDebounce(value: string, delay: number) {
+export function useDebounce(value: string | undefined, delay: number) {
   const [debouncedValue, setDebouncedValued] = useState(value);
 
   useEffect(() => {
--- src/plugins/backgrounds/giphy/GiphySettings.tsx
+++ src/plugins/backgrounds/giphy/GiphySettings.tsx
@@ -1,49 +1,39 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC } from 'react';
 
-import { useDebounce } from '../../../hooks';
 import { Props, defaultData } from './types';
+import { DebounceInput } from '../../shared';
 
-const GiphySettings: FC<Props> = ({ data = defaultData, setData }) => {
-  const [tag, setTag] = useState(data.tag);
-  const debouncedTag = useDebounce(tag, 500);
+const GiphySettings: FC<Props> = ({ data = defaultData, setData }) => (
+  <div className="GiphySettings">
+    <label>
+      Tag
+      <DebounceInput
+        type="text"
+        value={data.tag}
+        onChange={value => setData({ ...data, tag: value })}
+        wait={500}
+      />
+    </label>
+    <p className="info">Separate multiple tags with a commas</p>
 
-  useEffect(() => {
-    if (debouncedTag) {
-      setData({ ...data, tag: debouncedTag });
-    }
-  }, [debouncedTag]);
+    <label>
+      <input
+        type="checkbox"
+        checked={data.nsfw}
+        onChange={event => setData({ ...data, nsfw: !data.nsfw })}
+      />{' '}
+      Allow NSFW
+    </label>
 
-  return (
-    <div className="GiphySettings">
-      <label>
-        Tag
-        <input
-          type="text"
-          value={tag}
-          onChange={event => setTag(event.target.value)}
-        />
-      </label>
-      <p className="info">Separate multiple tags with a commas</p>
-
-      <label>
-        <input
-          type="checkbox"
-          checked={data.nsfw}
-          onChange={event => setData({ ...data, nsfw: !data.nsfw })}
-        />{' '}
-        Allow NSFW
-      </label>
-
-      <label>
-        <input
-          type="checkbox"
-          checked={data.expand}
-          onChange={event => setData({ ...data, expand: !data.expand })}
-        />{' '}
-        Stretch to fill screen
-      </label>
-    </div>
-  );
-};
+    <label>
+      <input
+        type="checkbox"
+        checked={data.expand}
+        onChange={event => setData({ ...data, expand: !data.expand })}
+      />{' '}
+      Stretch to fill screen
+    </label>
+  </div>
+);
 
 export default GiphySettings;
