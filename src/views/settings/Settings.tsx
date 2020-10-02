7f160b34c0c9f2133bf280c334321bd083d5edc4
Joel Shepherd <joel@swivid.ink>
2020-10-03 00:48:11 +1000

    A bit of polish

--- src/views/settings/Settings.tsx
+++ src/views/settings/Settings.tsx
@@ -1,19 +1,18 @@
 import React, { FC, memo, useCallback } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { useDispatch, useSelector } from 'react-redux';
-
+import { useDispatch } from 'react-redux';
 import { useKeyPress } from '../../hooks';
+import { useSelector } from '../../store';
 import { resetStore, toggleSettings } from '../../store/actions';
-import { dataStorage } from '../../store/storage';
 import { DataState } from '../../store/reducers/types';
 import { Icon } from '../shared';
 import Logo from '../shared/Logo';
 import Background from './Background';
 import Feedback from './Feedback';
 import Homepage from './Homepage';
+import './Settings.sass';
 import System from './System';
 import Widgets from './Widgets';
-import './Settings.sass';
 
 const Settings: FC = () => {
   const dispatch = useDispatch();
@@ -21,10 +20,14 @@ const Settings: FC = () => {
     dispatch,
   ]);
   const handleReset = useCallback(() => dispatch(resetStore()), [dispatch]);
-  const handleExport = async () => {
-    const { data } = useSelector((state) => state);
-    const jsonData = JSON.stringify(data);
-    const url = URL.createObjectURL(new Blob([jsonData], { type: 'octet/stream' }));
+  const data = useSelector((state) => state.data);
+
+  const handleExport = () => {
+    const json = JSON.stringify(data);
+    const url = URL.createObjectURL(
+      new Blob([json], { type: 'application/json' }),
+    );
+
     const a = document.createElement('a');
     document.body.appendChild(a);
     a.style.display = 'none';
@@ -32,26 +35,29 @@ const Settings: FC = () => {
     a.download = 'tabliss.json';
     a.click();
     window.URL.revokeObjectURL(url);
-  }
+    document.body.removeChild(a);
+  };
   const handleImport = () => {
     const input = document.createElement('input');
     document.body.appendChild(input);
+    input.style.display = 'none';
     input.type = 'file';
     input.addEventListener('change', function () {
       if (this.files) {
         const file = this.files[0];
         const reader = new FileReader();
         reader.addEventListener('load', (event) => {
-          if (event && event.target && event.target.result) {
+          if (event.target && event.target.result) {
             const state: DataState = JSON.parse(event.target.result as string);
             dispatch(resetStore(state));
           }
         });
         reader.readAsText(file);
       }
+      document.body.removeChild(input);
     });
     input.click();
-  }
+  };
 
   useKeyPress(handleToggleSettings, ['Escape']);
 
@@ -83,15 +89,9 @@ const Settings: FC = () => {
         </p>
 
         <p>
-          <a onClick={handleExport}>Export settings</a>
-        </p>
-
-        <p>
-          <a onClick={handleImport}>Import settings</a>
-        </p>
-
-        <p>
-          <a onClick={handleReset}>Reset to default</a>
+          <a onClick={handleImport}>Import</a>,{' '}
+          <a onClick={handleExport}>export</a> or{' '}
+          <a onClick={handleReset}>reset</a> settings
         </p>
 
         <p>
