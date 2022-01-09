8750f8baeec9819cdbaef0a44fb6b4d31526ed3e
Joel Shepherd <joel@swivid.ink>
2022-01-09 10:30:49 +1000

    Adding Brave search engine

--- src/plugins/widgets/search/engines.ts
+++ src/plugins/widgets/search/engines.ts
@@ -65,4 +65,9 @@ export const engines: Engine[] = [
     name: "MetaGer",
     search_url: "https://metager.de/meta/meta.ger3?eingabe={searchTerms}",
   },
+  {
+    key: "brave",
+    name: "Brave",
+    search_url: "https://search.brave.com/search?q={searchTerms}",
+  },
 ];
