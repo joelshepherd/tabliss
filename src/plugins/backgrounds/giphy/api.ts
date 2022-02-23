d3ddeccab1c191344750d182afe7eefd4f16f585
Sebastian <44152511+OneComputerGuy@users.noreply.github.com>
2022-02-22 20:17:59 -0500

    Updated to support webp instead of the direct (and undocumented) URL

--- src/plugins/backgrounds/giphy/api.ts
+++ src/plugins/backgrounds/giphy/api.ts
@@ -25,7 +25,7 @@ export async function getGif(
 
   loader.push();
   const res = await (await fetch(request)).json();
-  const data = await (await fetch(res.data.images.original.url)).blob();
+  const data = await (await fetch(res.data.images.original.webp)).blob();
   loader.pop();
 
   return {
