3dc1629cd57013d35d73ad628b2391a249a412bd
Joel Shepherd <joel@swivid.ink>
2022-04-10 00:54:26 +1000

    Adding theme set

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -10,6 +10,11 @@ const Dashboard: React.FC = () => {
   const background = useValue(db, "background");
   const theme = (background.display.luminosity ?? 0) > 0 ? "light" : "dark";
 
+  // Set init theme for pre settings load (see `target/<target>/index.html`)
+  React.useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <div className={`Dashboard fullscreen ${theme}`}>
       <Background />
