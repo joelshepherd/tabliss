7e6716868207cd82d14862cef6d199593c205b14
HFDan <58553089+HFDan@users.noreply.github.com>
2022-08-26 14:36:18 +0300

    Changed to an RNG method that is not biased. All lines should have and equal chance to display.

--- src/plugins/widgets/customText/CustomText.tsx
+++ src/plugins/widgets/customText/CustomText.tsx
@@ -4,13 +4,24 @@ import {defaultData, Props} from "./types";
 
 const CustomText: FC<Props> = ({data= defaultData}) => {
 
+  // Code for unbiased rand from https://pthree.org/2018/06/13/why-the-multiply-and-floor-rng-method-is-biased
+  const unbiasedRand = (range: number) => {
+    let max = Math.floor(2**32/range) * range;
+    let x;
+    do {
+      x = Math.floor(Math.random() * 2**32);
+    } while (x >= max);
+
+    return(x % range);
+  }
+
   let sep: string;
   if (data.atNewline)
     sep = "\n";
   else
     sep = data.separator;
 
-  const result = data.text.split(sep)[Math.floor(Math.random() * data.text.split(sep).length)];
+  const result = data.text.split(sep)[unbiasedRand(data.text.split(sep).length)];
 
   return (
     <div className="CustomText">
